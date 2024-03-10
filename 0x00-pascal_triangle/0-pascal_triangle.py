8e76151f33a07a7f3326f0164a5670f5ca61971a
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-10 16:36:19 +0000

    enforced pep8 coding style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,17 +2,24 @@
 
 """ A programs that implements Pascal's triangle."""
 
+
 triangle = []
+
+
 def baseline():
+    """Creates the first two rows of the Pascal's triangle."""
     global triangle
     triangle.append([1])
-    triangle.append([1,1])
+    triangle.append([1, 1])
+
 
 def sum_item(item, row):
+    """Creates a new row from the sum of the row above."""
     for index, value in enumerate(row):
-        if value == item and index < len(row) -1:
+        if value == item and index < len(row) - 1:
             next_item = row[index + 1]
             return item + next_item
+
 
 def pascal_triangle(n):
     """A function that implements Pascal's triangle.
@@ -35,7 +42,11 @@ def pascal_triangle(n):
         for num in range(0, size):
             copy = triangle[:]
             last_row = copy[-1]
-            new_row = [sum_item(value, last_row) for index, value in enumerate(last_row) if index != len(last_row) -1]
+            new_row = [
+                    sum_item(value, last_row)
+                    for index, value in enumerate(last_row)
+                    if index != len(last_row) - 1
+                    ]
             new_row.append(1)
             new_row.insert(0, 1)
             triangle.append(new_row)
