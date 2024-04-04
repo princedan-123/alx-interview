6bc5d518fcf11604f5a97dc74417dc2ad4bc1fc5
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-04 20:49:41 +0100

    task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A Python script checks if a dataset represents a valid UTF-8 encoding."""
+
+
+def validUTF8(data):
+    """
+        A function that validates a byte of data, checking if it is
+        a valid utf-8 encoding.
+        args:data(list) a list of integers.
+        return: True or False
+    """
+    for byte in data:
+        validate = None
+        if isinstance(byte, int):
+            bin = bin(byte)
+            binary = bin[2:]
+            if len(binary) == 7:
+                validate = True
+            if len(binary) > 7:
+                first_bit = binary[0]
+                second_bit = binary[1]
+                if first_bit == '1' and second_bit == '1':
+                    validate = True
+        else:
+            validate = False
+            return validate
+    return validate
