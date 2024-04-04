47d4852c356bf33ad504d857cac74ae18855d936
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-04 21:41:38 +0100

    just checking for single byte unicode (ascii)

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,19 +9,12 @@ def validUTF8(data):
         args:data(list) a list of integers.
         return: True or False
     """
+    validate = None
     for byte in data:
-        validate = False
         if isinstance(byte, int):
-            binary_string = bin(byte)
-            binary = binary_string[2:]
-            if len(binary) == 7:
+            if byte >= 0 and byte <= 127:
                 validate = True
-            if len(binary) > 7:
-                first_bit = binary[0]
-                second_bit = binary[1]
-                if first_bit == '1' and second_bit == '1':
-                    validate = True
-                else:
-                    validate = False 
-                    return validate
+            else:
+                validate = False
+                break
     return validate
