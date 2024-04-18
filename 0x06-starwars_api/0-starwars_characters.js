c06c8ce23a27c126d448161273f43eac3b68e86d
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-18 08:38:39 +0000

    implemented starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,15 +1,46 @@
 #!/usr/bin/node
+const request = require('request');
 
-const { request } = require('http');
-
-request = require('request')
-
-const id = parseInt(process.argv[2]);
-const url = `https://swapi.dev/api/films/${id}/`;
-
-request(url, (error, response, body) => {
-    if (response.statusCode === 200) {
-        console.log(body);
+const path = process.argv[2];
+const filmEndpoint = `https://swapi.dev/api/films/${path}`;
+function person (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (response.statusCode === 200) {
+        const person = JSON.parse(body);
+        const name = person.name;
+        resolve(name);
+      } else {
+        reject(error);
+      }
     }
+    );
+  }
+  );
+}
 
-})
+function getCharacters (filmEndpoint) {
+  return new Promise((resolve, reject) => {
+    request(filmEndpoint, (error, response, body) => {
+      if (response.statusCode === 200) {
+        const result = JSON.parse(body);
+        const characters = result.characters;
+        resolve(characters);
+      } else {
+        reject(error);
+      }
+    });
+  }
+  );
+}
+getCharacters(filmEndpoint).then((characters) => {
+  const listPromises = characters.map(person);
+  return Promise.all(listPromises);
+}
+).then((names) => {
+  names.forEach((name) => {
+    console.log(name);
+  });
+}).catch((error) => {
+  console.error('An error occured', error);
+});
