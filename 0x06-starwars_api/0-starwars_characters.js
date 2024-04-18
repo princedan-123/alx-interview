0c702e0485314577f1ce85e530212cc301f73f30
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-18 09:11:23 +0000

    added documentation

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,6 +3,11 @@ const request = require('request');
 
 const path = process.argv[2];
 const filmEndpoint = `https://swapi.dev/api/films/${path}`;
+
+/* Create a function for making request to each person endpoints
+ * in characters array
+ */
+
 function person (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
@@ -19,6 +24,10 @@ function person (url) {
   );
 }
 
+/* A funcion that makes a request to a film endpoint and returns an characters attribute
+ * which is an array of person endpoints in a particular file
+ */
+
 function getCharacters (filmEndpoint) {
   return new Promise((resolve, reject) => {
     request(filmEndpoint, (error, response, body) => {
@@ -33,6 +42,10 @@ function getCharacters (filmEndpoint) {
   }
   );
 }
+/* calling the getcharacter function to the the character and subsequently
+ * make a request to each person endpoint in the character array
+ */
+
 getCharacters(filmEndpoint).then((characters) => {
   const listPromises = characters.map(person);
   return Promise.all(listPromises);
